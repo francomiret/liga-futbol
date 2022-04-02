ee428d58b8ceda2b50cff95c9d2af0fb6892dcd9
franco <francomiret11@gmail.com>
2022-04-02 14:38:55 -0300

    feat: add goleadores table and tarjetas tables

--- src/app/goleadores/goleadores.component.ts
+++ src/app/goleadores/goleadores.component.ts
@@ -1,21 +1,103 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-goleadores',
   templateUrl: './goleadores.component.html',
   styleUrls: ['./goleadores.component.scss'],
 })
-export class GoleadoresComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class GoleadoresComponent {
+  displayedColumns = ['jugador', 'club', 'goles'];
+  dataSource = ELEMENT_DATA;
 }
-const matModules = [];
+
+export interface Goleadores {
+  club: string;
+  position: number;
+  jugador: string;
+  goles: number;
+}
+
+const ELEMENT_DATA: Goleadores[] = [
+  {
+    position: 1,
+    club: 'C.D. Barú',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 2,
+    club: 'C.A. General Urquiza',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 3,
+    club: 'C.A. Baylina A',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 4,
+    club: 'C.A. Baylina B',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 5,
+    club: 'D. Berduc',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 6,
+    club: 'S. Villa Clara',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 7,
+    club: 'C.A.H. de Hocker',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 8,
+    club: 'C.D. Hambis',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 9,
+    club: 'C.I. de Jubileo',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 10,
+    club: 'Las Palmas',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 11,
+    club: 'Azul Azul',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+  {
+    position: 12,
+    club: 'C.A. San Antonio',
+    jugador: 'Lihuen Segovia',
+    goles: 100,
+  },
+];
+const matModules = [MatTableModule];
 
 @NgModule({
   declarations: [GoleadoresComponent],
   exports: [GoleadoresComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, matModules],
 })
 export class GoleadoresModule {}
--- src/app/tarjetas/tarjetas.component.ts
+++ src/app/tarjetas/tarjetas.component.ts
@@ -1,21 +1,95 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-tarjetas',
   templateUrl: './tarjetas.component.html',
   styleUrls: ['./tarjetas.component.scss'],
 })
-export class TarjetasComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class TarjetasComponent {
+  displayedColumns = ['jugador', 'club', 'ta', 'tr'];
+  dataSource = ELEMENT_DATA;
 }
-const matModules = [];
+
+export interface Tarjetas {
+  club: string;
+  position: number;
+  jugador: string;
+  ta: number;
+  tr: number;
+}
+
+const ELEMENT_DATA: Tarjetas[] = [
+  {
+    position: 1,
+    club: 'C.D. Barú',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 2,
+    club: 'C.A. General Urquiza',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 3,
+    club: 'C.A. Baylina A',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 4,
+    club: 'C.A. Baylina B',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 5,
+    club: 'D. Berduc',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 6,
+    club: 'S. Villa Clara',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 7,
+    club: 'C.A.H. de Hocker',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 8,
+    club: 'C.D. Hambis',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+  {
+    position: 9,
+    club: 'C.I. de Jubileo',
+    jugador: 'Lihuen Segovia',
+    ta: 2,
+    tr: 10,
+  },
+];
+const matModules = [MatTableModule];
 
 @NgModule({
   declarations: [TarjetasComponent],
   exports: [TarjetasComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, matModules],
 })
 export class TarjetasModule {}
