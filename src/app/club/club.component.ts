b7fce64bd41c8242c678cbb3a0d1323dff4247e4
franco <francomiret11@gmail.com>
2022-04-08 21:17:55 -0300

    feat: update clubes view

--- src/app/club/club.component.ts
+++ src/app/club/club.component.ts
@@ -1,5 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { equipos } from 'src/models/test-data';
 
 @Component({
   selector: 'app-club',
@@ -8,13 +10,16 @@ import { Component, NgModule, OnInit } from '@angular/core';
 })
 export class ClubesComponent implements OnInit {
   constructor() {}
-
+  public clubes = equipos;
+  public clubClicked(nombre: string) {
+    console.log(nombre);
+  }
   ngOnInit(): void {}
 }
-
+const materialModules = [MatCardModule];
 @NgModule({
   declarations: [ClubesComponent],
   exports: [ClubesComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, materialModules],
 })
 export class ClubModule {}
