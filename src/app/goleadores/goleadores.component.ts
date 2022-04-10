28181c8a223197f007187ef9267b958cd28709fb
franco <francomiret11@gmail.com>
2022-04-10 09:45:34 -0300

    refactor: update goleadores table

--- src/app/goleadores/goleadores.component.ts
+++ src/app/goleadores/goleadores.component.ts
@@ -1,98 +1,64 @@
 import { CommonModule } from '@angular/common';
 import { Component, NgModule, OnInit } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { equipos, jugadores, partidos } from 'src/models/test-data';
 
 @Component({
   selector: 'app-goleadores',
   templateUrl: './goleadores.component.html',
   styleUrls: ['./goleadores.component.scss'],
 })
-export class GoleadoresComponent {
+export class GoleadoresComponent implements OnInit {
+  public goleadores: Goleador[] = [];
+  ngOnInit(): void {
+    const goleadoresId = Object.keys(this.getGoleadores());
+    for (let i = 0; i < goleadoresId.length; i++) {
+      let jugadorId = goleadoresId[i];
+      const goleador: Goleador = {
+        jugador: this.getJugadorName(jugadorId),
+        club: this.getEquipoJugador(jugadorId),
+        goles: this.getGoleadores()[jugadorId],
+        position: i + 1,
+      };
+      this.goleadores.push(goleador);
+    }
+  }
   displayedColumns = ['jugador', 'club', 'goles'];
-  dataSource = ELEMENT_DATA;
+
+  public getJugadorName(id: string) {
+    return jugadores.find((x) => x.id === id)?.nombre ?? '';
+  }
+
+  public getEquipoJugador(id: string) {
+    return (
+      equipos.find((x) => x.id === jugadores.find((x) => x.id === id)?.equipoId)
+        ?.nombre ?? ''
+    );
+  }
+
+  public getGoleadores() {
+    const allGoleadores: string[] = [];
+    let repetidos: Record<string, number> = {};
+
+    partidos.forEach((partido) => {
+      allGoleadores.push(...partido.golesLocalId);
+      allGoleadores.push(...partido.golesVisitanteId);
+    });
+
+    allGoleadores.forEach(function (numero) {
+      repetidos[numero] = (repetidos[numero] || 0) + 1;
+    });
+    return repetidos;
+  }
 }
 
-export interface Goleadores {
+export interface Goleador {
   club: string;
   position: number;
   jugador: string;
   goles: number;
 }
 
-const ELEMENT_DATA: Goleadores[] = [
-  {
-    position: 1,
-    club: 'C.D. Barú',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 2,
-    club: 'C.A. General Urquiza',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 3,
-    club: 'C.A. Baylina A',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 4,
-    club: 'C.A. Baylina B',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 5,
-    club: 'D. Berduc',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 6,
-    club: 'S. Villa Clara',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 7,
-    club: 'C.A.H. de Hocker',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 8,
-    club: 'C.D. Hambis',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 9,
-    club: 'C.I. de Jubileo',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 10,
-    club: 'Las Palmas',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 11,
-    club: 'Azul Azul',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-  {
-    position: 12,
-    club: 'C.A. San Antonio',
-    jugador: 'Lihuen Segovia',
-    goles: 100,
-  },
-];
 const matModules = [MatTableModule];
 
 @NgModule({
